4c6ef9c4771f465610cd7803f7fc6fc91fc90ec3
Stuart Mindt <stuart.mindt@madetech.com>
2021-04-13 11:51:04 +0100

    Pre commit test

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,10 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <a className="App-link" href="https://reactjs.org" target="_blank"
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
