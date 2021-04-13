17ba04c18ebaf9a41e04049e4d0cb297a07d48a2
Stuart Mindt <stuart.mindt@madetech.com>
2021-04-13 11:35:43 +0100

    Added cypress, eslint and prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.scss';
+import React from "react";
+import "./App.scss";
+import logo from "./logo.svg";
 
 function App() {
   return (
@@ -10,10 +10,7 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
+        <a className="App-link" href="https://reactjs.org" target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
