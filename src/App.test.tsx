349d5037badbb21a5f829fff9fced7615ec1fd7c
Stuart Mindt <stuart.mindt@madetech.com>
2021-04-16 14:03:41 +0100

    updating tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,14 @@ import { render, screen } from "@testing-library/react";
 import React from "react";
 import App from "./App";
 
-test("renders learn react link", () => {
+test("renders overview link", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Overview/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test("renders beacon records link", () => {
+  render(<App />);
+  const linkElement = screen.getByText(/Beacon records/i);
   expect(linkElement).toBeInTheDocument();
 });
