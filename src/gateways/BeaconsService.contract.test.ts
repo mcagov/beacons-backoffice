12a18b036e00ef765d625c667411b566f14da065
Zack Adlington <54271433+zackads@users.noreply.github.com>
2021-04-22 09:46:51 +0100

    Placeholder for a Beacons API contract test

---
+++ src/gateways/BeaconsService.contract.test.ts
@@ -0,0 +1,6 @@
+describe("The remote Beacons service", () => {
+  xit("Complies with the contract", () => {
+    // TODO: Call the live beacons service in staging or prod (?) and assert that the return value meets the
+    // contract expected by BeaconsGateway
+  });
+});
