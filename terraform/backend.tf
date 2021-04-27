a1572946215f76d7f9ede75abd2e6d30af7cec8e
Zack Adlington <54271433+zackads@users.noreply.github.com>
2021-04-27 13:32:35 +0100

    Separate DynamoDB locking table from other Beacons services
    Co-authored-by: Ninamma Rai <ninamma.rai@outlook.com>

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -3,7 +3,7 @@ terraform {
     bucket         = "mca-beacons-backoffice-terraform-state"
     key            = "global/s3/terraform.tfstate"
     region         = "eu-west-2"
-    dynamodb_table = "beacons-terraform-state-lock"
+    dynamodb_table = "beacons-backoffice-terraform-state-lock"
     encrypt        = true
   }
 }
