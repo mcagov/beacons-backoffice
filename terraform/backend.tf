5666bf6a0a53b031f2a975d8e7a4d911cc2993d5
Zack Adlington <54271433+zackads@users.noreply.github.com>
2021-04-27 17:27:44 +0100

    Review points with Matt

--- terraform/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "mca-beacons-backoffice-terraform-state"
-    key            = "global/s3/terraform.tfstate"
-    region         = "eu-west-2"
-    dynamodb_table = "beacons-backoffice-terraform-state-lock"
-    encrypt        = true
-  }
-}
--- terraform/cloudfront.tf
+++ terraform/cloudfront.tf
@@ -1,8 +1,8 @@
 resource "aws_cloudfront_distribution" "s3_distribution" {
   tags = module.beacons_label.tags
   origin {
-    domain_name            = aws_s3_bucket.backoffice-static.bucket_regional_domain_name
-    origin_id              = aws_s3_bucket.backoffice-static.bucket
+    domain_name = aws_s3_bucket.backoffice-static.bucket_regional_domain_name
+    origin_id   = aws_s3_bucket.backoffice-static.bucket
     s3_origin_config {
       origin_access_identity = aws_cloudfront_origin_access_identity.oai.cloudfront_access_identity_path
     }
@@ -26,7 +26,7 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
 
     viewer_protocol_policy = "redirect-to-https"
     min_ttl                = 0
-    default_ttl            = 3600 // 1 hour
+    default_ttl            = 3600  // 1 hour
     max_ttl                = 86400 // 1 day
   }
 
