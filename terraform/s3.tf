911914296eba8c91ec721864811c5ca4de85b2e5
Ninamma Rai <ninamma.rai@outlook.com>
2021-04-27 20:18:58 +0100

    Removing CORS rule as we don't need anything external to publicly access the backoffice

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -4,14 +4,6 @@ resource "aws_s3_bucket" "backoffice-static" {
   force_destroy = true
   acl           = "public-read"
 
-  cors_rule {
-    allowed_headers = ["*"]
-    allowed_methods = ["PUT", "POST"]
-    allowed_origins = ["*"]
-    expose_headers  = ["ETag"]
-    max_age_seconds = 3000
-  }
-
   website {
     index_document = "index.html"
     error_document = "error.html"
