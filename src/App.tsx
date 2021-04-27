d3929fd4441982011b21e183b06351ca64758628
Zack Adlington <54271433+zackads@users.noreply.github.com>
2021-04-27 16:45:25 +0100

    Add Azure AD auth to content in page body

--- src/App.tsx
+++ src/App.tsx
@@ -7,27 +7,32 @@ import "./App.scss";
 import Beacon from "./pages/beacon";
 import { BeaconsGateway } from "./gateways/BeaconsGateway";
 import Footer from "./components/Footer";
+import { AuthWrapper } from "./components/auth/AuthWrapper";
+import { RequireAuth } from "components/auth/RequireAuth";
 
 const App: FunctionComponent = () => {
   const beaconsGateway = new BeaconsGateway();
+
   return (
-    <>
+    <AuthWrapper>
       <Router>
         <Navigation />
-        <Switch>
-          <Route exact path="/">
-            <Home />
-          </Route>
-          <Route path="/beacon-records">
-            <BeaconRecords beaconsGateway={beaconsGateway} />
-          </Route>
-          <Route path="/beacon">
-            <Beacon />
-          </Route>
-        </Switch>
+        <RequireAuth>
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route path="/beacon-records">
+              <BeaconRecords beaconsGateway={beaconsGateway} />
+            </Route>
+            <Route path="/beacon">
+              <Beacon />
+            </Route>
+          </Switch>
+        </RequireAuth>
       </Router>
       <Footer />
-    </>
+    </AuthWrapper>
   );
 };
 
