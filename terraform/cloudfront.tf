dc758ebdd8b1f4b4f83b32b30f4e9c6cb8092826
Marten Wetterberg <marten.wetterberg@nyweb.nu>
2021-04-28 13:02:22 +0100

    Enforce SNI SSL certs, because otherwise it all BREAKS :(

--- terraform/cloudfront.tf
+++ terraform/cloudfront.tf
@@ -36,6 +36,7 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
 
   viewer_certificate {
     acm_certificate_arn = "arn:aws:acm:us-east-1:232705206979:certificate/91e17b33-5a3b-4ac9-a386-68711286c363"
+    ssl_support_method  = "sni-only"
   }
 
   restrictions {
