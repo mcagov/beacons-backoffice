e8dbb85cc6610cc25f6b5689e936bf6bb809acd5
Zack Adlington <54271433+zackads@users.noreply.github.com>
2021-04-28 12:30:53 +0100

    Fix imports in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 import Navigation from "components/Navigation";
-import BeaconRecords from "pages/beacon-records";
-import Home from "pages/home";
 import React, { FunctionComponent } from "react";
 import { HashRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.scss";
-import Beacon from "./pages/beacon";
 import { BeaconsGateway } from "./gateways/BeaconsGateway";
 import Footer from "./components/Footer";
 import { AuthWrapper } from "./components/auth/AuthWrapper";
 import { RequireAuth } from "components/auth/RequireAuth";
+import BeaconRecords from "./pages/BeaconRecords";
+import Beacon from "./pages/Beacon";
+import Home from "./pages/Home";
 
 const App: FunctionComponent = () => {
   const beaconsGateway = new BeaconsGateway();
