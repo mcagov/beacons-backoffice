5a686a2a9d9a707a09464b8d3ef6b9b6bcf28af8
Ninamma Rai <ninamma.rai@outlook.com>
2021-04-28 09:47:24 +0100

    Removing test that isn't adding value at the moment, might be used for user journey testing later?
    Co-authored-by: Zack Adlington <zadlington@gmail.com>

--- src/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import { render, screen } from "@testing-library/react";
-import React from "react";
-import App from "./App";
-
-test("renders beacon records link", () => {
-  render(<App />);
-  const linkElement = screen.getByText(/Beacon records/i);
-  expect(linkElement).toBeInTheDocument();
-});
