d9f95d22c3785a96783b4a1563b4112404c91b59
Zack Adlington <54271433+zackads@users.noreply.github.com>
2021-04-29 12:12:32 +0100

    Delete temp comments

--- src/views/SingleBeaconRecordView.test.tsx
+++ src/views/SingleBeaconRecordView.test.tsx
@@ -1,6 +1,3 @@
-// Higher-level tests that assert the sub-components that make up CertificateView work together correctly
-// E.g. "When I click on Tab X I can see a table containing Text Y"
-
 import { fireEvent, screen, render } from "@testing-library/react";
 import { SingleBeaconRecordView } from "./SingleBeaconRecordView";
 import React from "react";
