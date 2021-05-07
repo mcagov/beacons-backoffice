5d8166b3d2d13cd41e0057bcc8f7b0bdf012d0e8
Matthew Carr <matt.carr@madetech.com>
2021-05-07 11:23:56 +0100

    Update use definition to capture all use fields

--- src/fixtures/uses.fixture.ts
+++ src/fixtures/uses.fixture.ts
@@ -7,5 +7,15 @@ export const usesFixture: IUse[] = [
     purpose: Purposes.Commercial,
     activity: Activities.FishingVessel,
     moreDetails: "I take people out in my yacht.",
+    mainUse: true,
   }),
 ];
+
+export const useFixture = (mainUse: boolean): IUse => ({
+  id: "e00036c4-e3f4-46bb-aa9e-1d91870d9172",
+  environment: Environments.Maritime,
+  purpose: Purposes.Commercial,
+  activity: Activities.FishingVessel,
+  moreDetails: "I take people out in my yacht.",
+  mainUse,
+});
