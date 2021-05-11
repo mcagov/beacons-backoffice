1e78fda5196c91d38291e72ee3bb4803b9b24dde
Matthew Carr <matt.carr@madetech.com>
2021-05-11 12:34:45 +0100

    Render a use for a maritime environment

--- src/panels/UseSummaryPanel.test.tsx
+++ src/panels/UseSummaryPanel.test.tsx
@@ -32,6 +32,40 @@ describe("UseSummaryPanel", () => {
 
   it("should render an other activity use", async () => {
     use.activity = Activity.Other;
-    use.otherActivity;
+    use.otherActivity = "Gliding in the sea";
+
+    render(<UseSummaryPanel use={use} titlePrefix="Primary" />);
+
+    expect(
+      await screen.findByText("Primary use: GLIDING IN THE SEA (PLEASURE)")
+    ).toBeVisible();
+  });
+
+  it("should render a use for maritime use", async () => {
+    use.maxCapacity = 10;
+    use.vesselName = "Homeland";
+    use.beaconLocation = "In my carry bag";
+    use.portLetterNumber = "XYZ123";
+    use.homeport = "Newport";
+    use.areaOfOperation = "Whitesands Bay";
+    use.imoNumber = "IMO123";
+    use.ssrNumber = "SSR 123456";
+    use.rssNumber = "A12345";
+    use.officialNumber = "Fish and Ships";
+    use.rigPlatformLocation = "Scotland";
+
+    render(<UseSummaryPanel use={use} titlePrefix="Primary" />);
+
+    expect(await screen.findByText("10")).toBeVisible();
+    expect(await screen.findByText("HOMELAND")).toBeVisible();
+    expect(await screen.findByText("IN MY CARRY BAG")).toBeVisible();
+    expect(await screen.findByText("XYZ123")).toBeVisible();
+    expect(await screen.findByText("NEWPORT")).toBeVisible();
+    expect(await screen.findByText("WHITESANDS BAY")).toBeVisible();
+    expect(await screen.findByText("IMO123")).toBeVisible();
+    expect(await screen.findByText("SSR 123456")).toBeVisible();
+    expect(await screen.findByText("A12345")).toBeVisible();
+    expect(await screen.findByText("FISH AND SHIPS")).toBeVisible();
+    expect(await screen.findByText("SCOTLAND")).toBeVisible();
   });
 });
--- src/panels/UseSummaryPanel.tsx
+++ src/panels/UseSummaryPanel.tsx
@@ -1,6 +1,6 @@
 import { Card, CardContent, CardHeader } from "@material-ui/core";
 import { IField, PanelViewState } from "components/dataPanel/PanelViewState";
-import { IUse } from "entities/IUse";
+import { Activity, IUse } from "entities/IUse";
 import React, { FunctionComponent } from "react";
 
 interface UseSummaryPanelProps {
@@ -26,12 +26,38 @@ export const UseSummaryPanel: FunctionComponent<UseSummaryPanelProps> = ({
 };
 
 const getCardHeaderTitle = (titlePrefix: string, use: IUse): string => {
-  let title = `${titlePrefix} use: ${use.activity.replace(/_/, " ")} `;
+  let title = `${titlePrefix} use: `;
+  title +=
+    use.activity === Activity.Other
+      ? use.otherActivity?.toUpperCase()
+      : use.activity.replace(/_/, " ");
+
   if (use.purpose) {
-    title += `(${use.purpose})`;
+    title += ` (${use.purpose})`;
   }
 
   return title;
 };
 
-const getPanelFields = (use: IUse): IField[] => [];
+const getPanelFields = (use: IUse): IField[] => [
+  { key: "Max persons onboard", value: `${use?.maxCapacity}` },
+  { key: "Vessel name", value: use?.vesselName },
+  { key: "Beacon position", value: use?.beaconLocation },
+  { key: "Port Letter and Number (PLN)", value: use?.portLetterNumber },
+  { key: "Homeport", value: use?.homeport },
+  { key: "Area of operation", value: use?.areaOfOperation },
+  { key: "IMO number", value: `${use?.imoNumber}` },
+  { key: "UK Small Ships Register (SSR) number", value: use?.ssrNumber },
+  {
+    key: "Registry of Shipping and Seamen (RSS) number",
+    value: use?.rssNumber,
+  },
+  {
+    key: "Vessel official number",
+    value: use?.officialNumber,
+  },
+  {
+    key: "Windfarm or rig platform location",
+    value: use?.rigPlatformLocation,
+  },
+];
