937c8bfc896109e50c9418054de0b63adf625c82
Matthew Carr <matt.carr@madetech.com>
2021-05-11 11:26:22 +0100

    Update uses component to inject the uses gateway

--- src/panels/UsesSummaryPanel.test.tsx
+++ src/panels/UsesSummaryPanel.test.tsx
@@ -1,11 +1,22 @@
 import { render, screen } from "@testing-library/react";
 import { usesFixture } from "fixtures/uses.fixture";
+import { IUsesGateway } from "gateways/IUsesGateway";
 import React from "react";
 import { UsesSummaryPanel } from "./UsesSummaryPanel";
 
 describe("Uses Summary Panel", () => {
+  let gateway: IUsesGateway;
+  let beaconId: string;
+
+  beforeEach(() => {
+    gateway = {
+      getUses: jest.fn().mockResolvedValue(usesFixture),
+    };
+    beaconId = "1";
+  });
+
   it("should display the owners details", async () => {
-    render(<UsesSummaryPanel uses={usesFixture} />);
+    render(<UsesSummaryPanel usesGateway={gateway} beaconId={beaconId} />);
 
     expect(await screen.findByText(/Primary Use/i)).toBeVisible();
   });
--- src/panels/UsesSummaryPanel.tsx
+++ src/panels/UsesSummaryPanel.tsx
@@ -1,6 +1,7 @@
 import { Card, CardContent, CardHeader } from "@material-ui/core";
+import { IUse } from "entities/IUse";
 import { IUsesGateway } from "gateways/IUsesGateway";
-import { FunctionComponent } from "react";
+import { FunctionComponent, useEffect, useState } from "react";
 
 interface UsesSummaryPanelProps {
   usesGateway: IUsesGateway;
@@ -11,6 +12,29 @@ export const UsesSummaryPanel: FunctionComponent<UsesSummaryPanelProps> = ({
   usesGateway,
   beaconId,
 }: UsesSummaryPanelProps): JSX.Element => {
+  const [uses, setUses] = useState<IUse[]>();
+
+  useEffect((): (() => void) => {
+    let isMounted = true;
+    const fetchBeacon = async (id: string) => {
+      try {
+        const uses = await usesGateway.getUses(id);
+
+        if (isMounted) {
+          setUses(uses);
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchBeacon(beaconId);
+
+    return () => {
+      isMounted = false;
+    };
+  }, [beaconId, usesGateway]);
+
   return (
     <Card>
       <CardContent>
