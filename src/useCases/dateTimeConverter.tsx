5890d543c303bdf4ac8cf32c4d168f0f71c64a32
Zack Adlington <54271433+zackads@users.noreply.github.com>
2021-05-11 12:22:22 +0100

    Delete unused datetime converter fn

--- src/useCases/dateTimeConverter.tsx
+++ src/useCases/dateTimeConverter.tsx
@@ -1,4 +1,1 @@
-export const shortISOFormat = (dateString: string) =>
-  new Date(dateString).toISOString().slice(0, 16);
-
 export const yyyyMmDdFormat = (isoString: string) => isoString.slice(0, 10);
