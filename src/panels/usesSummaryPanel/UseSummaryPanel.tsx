a7a4b9f244c752947a7bd77a5d58f3fcf005913b
Matthew Carr <matt.carr@madetech.com>
2021-05-12 08:57:39 +0100

    Remove unused folders

--- src/panels/usesSummaryPanel/UseSummaryPanel.tsx
+++ src/panels/usesSummaryPanel/UseSummaryPanel.tsx
@@ -1,6 +1,7 @@
 import { Card, CardContent, CardHeader } from "@material-ui/core";
 import { Activities, Environments, IUse } from "entities/IUse";
 import React, { FunctionComponent, ReactNode } from "react";
+import { AviationSummary } from "./AviationSummary";
 import { MaritimeSummary } from "./MaritimeSummary";
 
 interface UseSummaryPanelProps {
