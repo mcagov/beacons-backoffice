0fae9c922b030949b250ae3e84ec5c4892027d11
Zack Adlington <54271433+zackads@users.noreply.github.com>
2021-05-12 16:04:35 +0100

    Add usesGatewayDouble to SingleBeaconRecordPage test

--- src/views/SingleBeaconRecordView.test.tsx
+++ src/views/SingleBeaconRecordView.test.tsx
@@ -2,10 +2,12 @@ import { render, screen } from "@testing-library/react";
 import React from "react";
 import { beaconFixture } from "../fixtures/beacons.fixture";
 import { IBeaconsGateway } from "../gateways/IBeaconsGateway";
+import { IUsesGateway } from "../gateways/IUsesGateway";
 import { SingleBeaconRecordView } from "./SingleBeaconRecordView";
 
 describe("Beacon record page", () => {
   let beaconsGatewayDouble: IBeaconsGateway;
+  let usesGatewayDouble: IUsesGateway;
 
   beforeEach(() => {
     beaconsGatewayDouble = {
@@ -13,12 +15,17 @@ describe("Beacon record page", () => {
       getAllBeacons: jest.fn(),
       updateBeacon: jest.fn(),
     };
+
+    usesGatewayDouble = {
+      getUses: jest.fn(),
+    };
   });
 
   it("Displays beacon's hex ID", async () => {
     render(
       <SingleBeaconRecordView
         beaconsGateway={beaconsGatewayDouble}
+        usesGateway={usesGatewayDouble}
         beaconId={beaconFixture.id}
       />
     );
@@ -31,6 +38,7 @@ describe("Beacon record page", () => {
     render(
       <SingleBeaconRecordView
         beaconsGateway={beaconsGatewayDouble}
+        usesGateway={usesGatewayDouble}
         beaconId={beaconFixture.id}
       />
     );
