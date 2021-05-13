7fc47e9a47472a0c3264c00725a4efd11ef11162
Zack Adlington <54271433+zackads@users.noreply.github.com>
2021-05-13 15:56:37 +0100

    Implement BeaconRequestMapper

--- src/gateways/mappers/BeaconRequestMapper.ts
+++ src/gateways/mappers/BeaconRequestMapper.ts
@@ -4,3 +4,49 @@ import { IBeaconRequest } from "./IBeaconRequest";
 export interface IBeaconRequestMapper {
   map: (beaconId: string, beacon: Partial<IBeacon>) => IBeaconRequest;
 }
+
+export class BeaconRequestMapper {
+  public map(beaconId: string, beacon: Partial<IBeacon>): IBeaconRequest {
+    return {
+      data: {
+        type: "beacon",
+        id: beaconId,
+        attributes: {
+          ...(beacon.hexId && {
+            hexId: beacon.hexId,
+          }),
+          ...(beacon.type && {
+            type: beacon.type,
+          }),
+          ...(beacon.protocolCode && {
+            protocolCode: beacon.protocolCode,
+          }),
+          ...(beacon.registeredDate && {
+            registeredDate: beacon.registeredDate,
+          }),
+          ...(beacon.status && {
+            status: beacon.status,
+          }),
+          ...(beacon.manufacturer && {
+            manufacturer: beacon.manufacturer,
+          }),
+          ...(beacon.model && {
+            model: beacon.model,
+          }),
+          ...(beacon.manufacturerSerialNumber && {
+            manufacturerSerialNumber: beacon.manufacturerSerialNumber,
+          }),
+          ...(beacon.chkCode && {
+            chkCode: beacon.chkCode,
+          }),
+          ...(beacon.batteryExpiryDate && {
+            batteryExpiryDate: beacon.batteryExpiryDate,
+          }),
+          ...(beacon.lastServicedDate && {
+            lastServicedDate: beacon.lastServicedDate,
+          }),
+        },
+      },
+    };
+  }
+}
