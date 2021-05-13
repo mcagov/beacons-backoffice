f03e1d99a785d499fd0c168ac6484df1f73e8175
Zack Adlington <54271433+zackads@users.noreply.github.com>
2021-05-13 16:43:59 +0100

    Refactor to use simple if statements instead of complicated ES syntax

--- src/gateways/mappers/BeaconRequestMapper.ts
+++ src/gateways/mappers/BeaconRequestMapper.ts
@@ -7,45 +7,29 @@ export interface IBeaconRequestMapper {
 
 export class BeaconRequestMapper {
   public map(beaconId: string, beacon: Partial<IBeacon>): IBeaconRequest {
+    const attributes: Record<string, string> = {};
+
+    if (beacon.hexId) attributes.hexId = beacon.hexId;
+    if (beacon.type) attributes.type = beacon.type;
+    if (beacon.protocolCode) attributes.protocolCode = beacon.protocolCode;
+    if (beacon.registeredDate)
+      attributes.registeredDate = beacon.registeredDate;
+    if (beacon.status) attributes.status = beacon.status;
+    if (beacon.manufacturer) attributes.manufacturer = beacon.manufacturer;
+    if (beacon.model) attributes.model = beacon.model;
+    if (beacon.manufacturerSerialNumber)
+      attributes.manufacturerSerialNumber = beacon.manufacturerSerialNumber;
+    if (beacon.chkCode) attributes.chkCode = beacon.chkCode;
+    if (beacon.batteryExpiryDate)
+      attributes.batteryExpiryDate = beacon.batteryExpiryDate;
+    if (beacon.lastServicedDate)
+      attributes.lastServicedDate = beacon.lastServicedDate;
+
     return {
       data: {
         type: "beacon",
         id: beaconId,
-        attributes: {
-          ...(beacon.hexId && {
-            hexId: beacon.hexId,
-          }),
-          ...(beacon.type && {
-            type: beacon.type,
-          }),
-          ...(beacon.protocolCode && {
-            protocolCode: beacon.protocolCode,
-          }),
-          ...(beacon.registeredDate && {
-            registeredDate: beacon.registeredDate,
-          }),
-          ...(beacon.status && {
-            status: beacon.status,
-          }),
-          ...(beacon.manufacturer && {
-            manufacturer: beacon.manufacturer,
-          }),
-          ...(beacon.model && {
-            model: beacon.model,
-          }),
-          ...(beacon.manufacturerSerialNumber && {
-            manufacturerSerialNumber: beacon.manufacturerSerialNumber,
-          }),
-          ...(beacon.chkCode && {
-            chkCode: beacon.chkCode,
-          }),
-          ...(beacon.batteryExpiryDate && {
-            batteryExpiryDate: beacon.batteryExpiryDate,
-          }),
-          ...(beacon.lastServicedDate && {
-            lastServicedDate: beacon.lastServicedDate,
-          }),
-        },
+        attributes: attributes,
       },
     };
   }
