7efd6ba45950e897cbb023ec1460b9843063d540
Ninamma Rai <ninamma.rai@outlook.com>
2021-05-14 16:58:25 +0100

    WIP: getting user's access token and making a request to MS graph (just to see what happens)

--- src/components/auth/AuthWrapper.tsx
+++ src/components/auth/AuthWrapper.tsx
@@ -14,10 +14,39 @@ const configuration: Configuration = {
 
 const pca = new PublicClientApplication(configuration);
 
+const account = pca.getAllAccounts()[0];
+
+const accessTokenRequest = {
+  scopes: ["user.read"],
+  account: account,
+};
+
+pca.acquireTokenSilent(accessTokenRequest).then((accessTokenResponse) => {
+  let accessToken = accessTokenResponse.accessToken;
+  console.log(accessToken);
+  console.log(JSON.parse(atob(accessToken.split(".")[1])));
+
+  const headers = new Headers();
+  const bearer = "Bearer " + accessToken;
+  headers.append("Authorization", bearer);
+  const options = {
+    method: "GET",
+    headers: headers,
+  };
+  var graphEndpoint = "https://graph.microsoft.com/v1.0/me";
+
+  fetch(graphEndpoint, options).then((response) => {
+    console.log(response.json());
+  });
+});
+
 interface AuthWrapperProps {
   children: React.ReactNode;
 }
 
 export const AuthWrapper: FunctionComponent<AuthWrapperProps> = ({
   children,
-}: AuthWrapperProps) => <MsalProvider instance={pca}>{children}</MsalProvider>;
+}: AuthWrapperProps) => {
+  console.log("here");
+  return <MsalProvider instance={pca}>{children}</MsalProvider>;
+};
