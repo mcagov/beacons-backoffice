2c3df1a3c213c90b691e51ea48ead5cbcbab6ea6
Zack Adlington <54271433+zackads@users.noreply.github.com>
2021-05-14 16:28:54 +0100

    Fix diffObjValues to handle empty strings

--- src/utils/core.test.ts
+++ src/utils/core.test.ts
@@ -1,40 +1,104 @@
 import { diffObjValues } from "./core";
 
 describe("diffObjValues()", () => {
-  it("when passed an empty object returns an empty object", () => {
-    const base = {};
-    const comparator = {};
+  const testCases = [
+    {
+      base: {},
+      comparator: {},
+      expectedOutput: {},
+      description: "when passed an empty object returns an empty object",
+    },
+    {
+      base: { a: "b" },
+      comparator: { a: "b" },
+      expectedOutput: {},
+      description: "when passed two identical objects, returns an empty object",
+    },
+    {
+      base: { a: "b" },
+      comparator: { a: "c" },
+      expectedOutput: { a: "c" },
+      description:
+        "when passed two different objects, returns the changed properties",
+    },
+    {
+      base: { a: "b", b: "d" },
+      comparator: { a: "c", b: "e" },
+      expectedOutput: { a: "c", b: "e" },
+      description:
+        "when passed two other different objects, returns the changed properties",
+    },
+    {
+      base: { a: "b", c: { d: "e" } },
+      comparator: { a: "b", c: { d: "f" } },
+      expectedOutput: { c: { d: "f" } },
+      description:
+        "when passed a deep object, returns the changed properties all the way down",
+    },
+    {
+      base: { a: "b", c: { d: "e", f: "g" } },
+      comparator: { a: "b", c: { d: "e", f: "h" } },
+      expectedOutput: { c: { f: "h" } },
+      description:
+        "when passed another deep object, returns the changed properties all the way down",
+    },
+    {
+      base: { a: "b", c: { d: "e", f: { g: "h" } } },
+      comparator: { a: "b", c: { d: "e", f: { g: "i" } } },
+      expectedOutput: { c: { f: { g: "i" } } },
+      description:
+        "when passed a three-deep object, returns the changed properties all the way down",
+    },
+    {
+      base: { a: [1, 2, 3] },
+      comparator: { a: [4, 5, 6] },
+      expectedOutput: { a: [4, 5, 6] },
+      description: "handles arrays",
+    },
+    {
+      base: { a: [1, 2, 3], b: "c" },
+      comparator: { a: [1, 2, 3], b: [4, 5, 6] },
+      expectedOutput: { b: [4, 5, 6] },
+      description: "handles other arrays",
+    },
+    {
+      base: { a: undefined, c: "d" },
+      comparator: { a: "b", c: "d" },
+      expectedOutput: { a: "b" },
+      description: "handles undefined",
+    },
+    {
+      base: { a: "b" },
+      comparator: { a: "" },
+      expectedOutput: { a: "" },
+      description: "handles empty strings",
+    },
+    {
+      base: { a: { b: "c" } },
+      comparator: { a: { b: "" } },
+      expectedOutput: { a: { b: "" } },
+      description: "handles nested empty strings",
+    },
+    {
+      base: { a: { b: "c" } },
+      comparator: { a: { b: "" } },
+      expectedOutput: { a: { b: "" } },
+      description: "handles nested empty strings",
+    },
+    {
+      base: { a: { b: "c" }, d: "e" },
+      comparator: { a: { b: "" }, d: "" },
+      expectedOutput: { a: { b: "" }, d: "" },
+      description: "handles more nested empty strings",
+    },
+  ];
 
-    const changedValues = diffObjValues(base, comparator);
-
-    expect(changedValues).toStrictEqual({});
-  });
-
-  it("when passed two identical objects, returns an empty object", () => {
-    const base = { a: "b" };
-    const comparator = { a: "b" };
-
-    const changedValues = diffObjValues(base, comparator);
-
-    expect(changedValues).toStrictEqual({});
-  });
-
-  it("when passed two different objects, returns the changed properties", () => {
-    const base = { a: "b" };
-    const comparator = { a: "c" };
-
-    const changedValues = diffObjValues(base, comparator);
-
-    expect(changedValues).toStrictEqual({ a: "c" });
-  });
-
-  it("when passed two other different objects, returns the changed properties", () => {
-    const base = { a: "b", b: "d" };
-    const comparator = { a: "c", b: "e" };
-
-    const changedValues = diffObjValues(base, comparator);
-
-    expect(changedValues).toStrictEqual({ a: "c", b: "e" });
+  testCases.forEach((test) => {
+    it(`${test.description}`, () => {
+      expect(diffObjValues(test.base, test.comparator)).toStrictEqual(
+        test.expectedOutput
+      );
+    });
   });
 
   it("when passed a comparator with a different key to the base, throws an error", () => {
@@ -44,59 +108,5 @@ describe("diffObjValues()", () => {
     const diffIncomparableObjects = () => diffObjValues(base, comparator);
 
     expect(diffIncomparableObjects).toThrowError();
-  });
-
-  it("when passed a deep object, returns the changed properties all the way down", () => {
-    const base = { a: "b", c: { d: "e" } };
-    const comparator = { a: "b", c: { d: "f" } };
-
-    const changedValues = diffObjValues(base, comparator);
-
-    expect(changedValues).toStrictEqual({ c: { d: "f" } });
-  });
-
-  it("when passed another deep object, returns the changed properties all the way down", () => {
-    const base = { a: "b", c: { d: "e", f: "g" } };
-    const comparator = { a: "b", c: { d: "e", f: "h" } };
-
-    const changedValues = diffObjValues(base, comparator);
-
-    expect(changedValues).toStrictEqual({ c: { f: "h" } });
-  });
-
-  it("when passed a three-deep object, returns the changed properties all the way down", () => {
-    const base = { a: "b", c: { d: "e", f: { g: "h" } } };
-    const comparator = { a: "b", c: { d: "e", f: { g: "i" } } };
-
-    const changedValues = diffObjValues(base, comparator);
-
-    expect(changedValues).toStrictEqual({ c: { f: { g: "i" } } });
-  });
-
-  it("handles arrays", () => {
-    const base = { a: [1, 2, 3] };
-    const comparator = { a: [4, 5, 6] };
-
-    const changedValues = diffObjValues(base, comparator);
-
-    expect(changedValues).toStrictEqual({ a: [4, 5, 6] });
-  });
-
-  it("handles other arrays", () => {
-    const base = { a: [1, 2, 3], b: "c" };
-    const comparator = { a: [1, 2, 3], b: [4, 5, 6] };
-
-    const changedValues = diffObjValues(base, comparator);
-
-    expect(changedValues).toStrictEqual({ b: [4, 5, 6] });
-  });
-
-  it("handles undefined", () => {
-    const base = { a: undefined, c: "d" };
-    const comparator = { a: "b", c: "d" };
-
-    const changedValues = diffObjValues(base, comparator);
-
-    expect(changedValues).toStrictEqual({ a: "b" });
   });
 });
