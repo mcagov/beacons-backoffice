b7c966e2f426715636b17db7f27aac9309da928a
Matthew Carr <matt.carr@madetech.com>
2021-08-10 09:47:59 +0100

    Commit fixture

---
+++ src/gateways/beacons/BeaconsGatewaySearchResult.fixture.ts
@@ -0,0 +1,43 @@
+import { deepFreeze } from "../../utils";
+
+export const beaconsGatewayFixture = deepFreeze({
+  meta: {
+    count: 3,
+    pageSize: 1,
+  },
+  data: [
+    {
+      type: "beaconSearchResult",
+      id: "97b306aa-cbd0-4f09-aa24-2d876b983efb",
+      attributes: {
+        hexId: "Hex me",
+        beaconStatus: "NEW",
+        lastUpdatedDate: "2020-02-01T00:00",
+        beaconUse: "Sailing, Kayaking",
+        ownerName: "Vice-Admiral Horatio Nelson, 1st Viscount Nelson",
+      },
+    },
+    {
+      type: "beaconSearchResult",
+      id: "97b306aa-cbd0-4f09-aa24-2d876b983efc",
+      attributes: {
+        hexId: "Hex me difficultly",
+        beaconStatus: "NEW",
+        lastUpdatedDate: "2020-02-01T00:00",
+        beaconUse: "Motoring",
+        ownerName: "Vice-Admiral Horatio Nelson, 1st Viscount Nelson",
+      },
+    },
+    {
+      type: "beaconSearchResult",
+      id: "97b306aa-cbd0-4f09-aa24-2d876b983efd",
+      attributes: {
+        hexId: "Hex me beacon",
+        beaconStatus: "NEW",
+        lastUpdatedDate: "2020-02-01T00:00",
+        beaconUse: "Vesseling",
+        ownerName: "Vice-Admiral Horatio Nelson, 1st Viscount Nelson",
+      },
+    },
+  ],
+});
