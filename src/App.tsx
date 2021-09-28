83c756afbe0c113128a34ff94b3db2b67ee35fbc
Ninamma Rai <32230328+Ninamma@users.noreply.github.com>
2021-09-28 15:06:37 +0100

    Add notesRequestMapper when instantiating NotesGateway

--- src/App.tsx
+++ src/App.tsx
@@ -18,6 +18,7 @@ import { applicationConfig } from "./config";
 import { BeaconRequestMapper } from "./gateways/mappers/BeaconRequestMapper";
 import { BeaconResponseMapper } from "./gateways/mappers/BeaconResponseMapper";
 import { LegacyBeaconResponseMapper } from "./gateways/mappers/LegacyBeaconResponseMapper";
+import { NotesRequestMapper } from "./gateways/mappers/NotesRequestMapper";
 import { NotesResponseMapper } from "./gateways/mappers/NotesResponseMapper";
 import { NotesGateway } from "./gateways/notes/NotesGateway";
 import { BeaconRecordsListView } from "./views/BeaconRecordsListView";
@@ -50,7 +51,12 @@ const App: FunctionComponent = () => {
   );
   const usesGateway = new UsesGateway(beaconResponseMapper, authGateway);
   const notesResponseMapper = new NotesResponseMapper();
-  const notesGateway = new NotesGateway(notesResponseMapper, authGateway);
+  const notesRequestMapper = new NotesRequestMapper();
+  const notesGateway = new NotesGateway(
+    notesResponseMapper,
+    notesRequestMapper,
+    authGateway
+  );
 
   const SingleBeaconRecordViewWithParam: FunctionComponent = () => {
     const { id } = useParams<ResourceParams>();
